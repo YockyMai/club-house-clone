f82dd5daec95f3b8c625657ab655d2b0410801ad
Grigoriev Valerii <g.valera15102003@gmail.com>
2022-03-16 01:02:34 +0500

    Сделал компонент с комнатами ConversationCart

---
+++ components/ConversationCart/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Avatar} from '../Avatar';
+
+import styles from './ConversationCart.module.scss';
+import whiteBlockStyles from '../WhiteBlock/WhiteBlock.module.scss';
+import clsx from 'clsx';
+
+interface ConversationsCartProps {
+    title: string,
+    avatars?: string[],
+    guests?: string[],
+    speakersCount: number,
+    guestsCount: number
+}
+
+export const ConversationCard: React.FC<ConversationsCartProps> =
+    ({
+         avatars = [],
+         guests = [],
+         speakersCount,
+         guestsCount,
+         title
+     }) => {
+        return (
+            <div className={clsx(whiteBlockStyles.block, styles.card, 'mb-30')}>
+                <h4 className={styles.title}>{title}</h4>
+                <div className={clsx('d-flex mt-10', styles.content)}>
+                    <div className={styles.avatars}>
+                        {avatars.map((url, i) => (
+                            <Avatar
+                                key={url}
+                                width="45px"
+                                height="45px"
+                                src={url}
+                                className={avatars.length > 1 && i === avatars.length - 1 ? 'lastAvatar' : ''}
+                            />
+                        ))}
+                    </div>
+                    <div className={clsx(styles.info, 'ml-10')}>
+                        <ul className={styles.users}>
+                            {guests.map((name, i) => (
+                                <li key={name + i}>
+                                    {name} <img src="/static/cloud.png" alt="Cloud" width={14} height={14}/>
+                                </li>
+                            ))}
+                        </ul>
+                        <ul className={styles.details}>
+                            <li>
+                                {guestsCount} <img src="/static/user.svg" alt="Users count" width={12} height={12}/>
+                            </li>
+                            <li>
+                                {speakersCount}
+                                <img
+                                    className="ml-5"
+                                    src="/static/message.svg"
+                                    alt="Users count"
+                                    width={12}
+                                    height={12}
+                                />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    };
--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import {Header} from "../components/Header";
+import {Button} from "../components/Button";
+import {ConversationCard} from "../components/ConversationCart";
 
-const RoomsPage = () => {
+const RoomsPage: React.FC = () => {
+    const Avatars = [
+        "https://sun9-60.userapi.com/impg/jeXfQKE1Ur1hbY1O9ZlX4qR-od7S1nWP9FoXRQ/0g7ygxIa5PE.jpg?size=1620x2160&quality=96&sign=2e05d1c0a2e900fb1bb147719d5461c3&type=album",
+        "https://cs12.pikabu.ru/post_img/2021/05/14/6/1620980169172670250.jpg",
+    ]
+    const guests = [
+        'Давид Арзуманян',
+        'Эвелина Тагирова',
+    ]
     return (
         <div>
             <Header/>
             <div className="container">
-                <h1>
-                    Тут будет главная страница приложения!
-                </h1>
+                <div className="d-flex align-items-center justify-content-between mt-60">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h1 className="">
+                            All conversations
+                        </h1>
+                        <Button className="ml-20" color={"gray"}>
+                            Explore
+                        </Button>
+                    </div>
+                    <Button color={"green"}>
+                        Start a room
+                    </Button>
+                </div>
+                <div className="d-flex justify-content-between flex-auto">
+                    <ConversationCard title={'Как найти идеальную девушку?'} speakersCount={2} guestsCount={24}
+                                      avatars={Avatars} guests={guests}/>
+                </div>
             </div>
-
         </div>
     );
 };
